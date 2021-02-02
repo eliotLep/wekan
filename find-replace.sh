a218b8d14e23b590a4adba23c848b91f6a4ed1b7
shishi42 <benriv4272@gmail.com>
2021-02-02 16:30:21 +0100

    giga clean

--- find-replace.sh
+++
@@ -1,5 +0,0 @@
-
-# Recursive find/replace.
-# Syntax: ./find-replace.sh searchtext replacetext
-
-egrep -lRZ '$1' . | xargs -0 -l sed -i -e 's/$1/$2/g'
--- releases/rebuild-docs.sh
+++
@@ -1,9 +0,0 @@
-# Generate docs.
-
-#mkdir -p public/api
-python3 ./openapi/generate_openapi.py --release $(git describe --tags --abbrev=0) > ./public/api/wekan.yml
-api2html -c ./public/logo-header.png -o ./public/api/wekan.html ./public/api/wekan.yml
-
-# Copy docs to bundle
-#cp -pR ./public/api ~/repos/wekan/.build/bundle/programs/web.browser/app/
-#cp -pR ./public/api ~/repos/wekan/.build/bundle/programs/web.browser.legacy/app/
