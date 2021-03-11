dc2cda7452e61e8e882271d69c90ef95769dd8fd
eliotLep <eliot.lepoittevin@gmail.com>
2021-03-11 12:21:31 +0100

    mongo acces

--- client/components/chart/chart.js
+++ client/components/chart/chart.js
@@ -4,13 +4,131 @@ const { calculateIndex } = Utils;
 
 //const Chart = require('chart.js');
 import Chart from 'chart.js';
+import Mongo from 'meteor/mongo'
+
+
+
+
+class Sprint {
+  constructor(receivedAt,dueAt){
+    this.receivedAt = receivedAt;
+    this.dueAt = dueAt;
+    this.numTasksTot = 0;
+    this.numTasksDone = 0;
+  }
+
+  addTaskTot(nbTasks){
+    this.numTasksTot+=nbTasks;
+  }
+  setTasksTot(nbTasks){
+    this.numTasksTot=nbTasks;
+  }
+  remTaskTot(nbTasks){
+    this.numTasksTot-=nbTasks;
+  }
+
+  setTasksDone(nbTasks){
+    this.numTasksDone=nbTasks;
+  }
+  addTaskDone(nbTasks){
+    this.numTasksDone+=nbTasks;
+  }
+  remTaskDone(nbTasks){
+    this.numTasksDone-=nbTasks;
+  }
+
+}
+
 
 BlazeComponent.extendComponent({
+
+
+
   onRendered() {
-    console.log('cest un test');
+
+    //on recupere toutes les cartes
+    const cardsDueTime = Cards.find().fetch();
+
+    //contiendra la date des sprints
+    var sprints = new Array();
+
+
+    for(var i=0;i<cardsDueTime.length;i++){
+      var item = cardsDueTime[i];
+
+      if(item.dueAt==null || item.dueAt==undefined || item.receivedAt==null || item.receivedAt==undefined){
+        continue;
+      }
+      var receivedAt = item.receivedAt;
+      var dueAt = item.dueAt;
+
+      var test=0;
+      for(var j=0;j<sprints.length;j++){
+        var itemSprint = sprints[j];
+        if( itemSprint.dueAt.getDay()==dueAt.getDay() ){
+          test=1;
+          break;
+        }
+      }
+      if(test==0){
+        sprints.push( new Sprint(receivedAt,dueAt) );
+      }
+
+    }
+
+    //on setup les valeurs global du projet
+    var totalTaskCount=0;
+    var dateStartProject;
+    var dateEndProject;
+    if(sprints.length>0){
+      dateStartProject=sprints[0].receivedAt;
+      dateEndProject=sprints[0].dueAt;
+    }
+    for(var i=0;i<sprints.length;i++){
+      if(dateStartProject>sprints[i].receivedAt){
+        dateStartProject = sprints[i].receivedAt;
+      }
+      if(dateEndProject<sprints[i].dueAt){
+        dateEndProject = sprints[i].dueAt;
+      }
+
+    }
+
+
+    for(var i=0;i<sprints.length;i++){
+      var item = sprints[i];
+
+      //on recupere les tasks de ce sprint.
+      var sprintTasksTot = Cards.find( {receivedAt : {"$gte": item.receivedAt,"$lt": item.dueAt} } ).fetch();
+
+      totalTaskCount+=sprintTasksTot.length;
+
+      //on recupere les tasks terminé pour ce sprint
+      var sprintTasksDone=new Array();
+      for(var j=0;j<sprintTasksTot.length;j++){
+        if(sprintTasksTot[j].endAt!=null && sprintTasksTot[j].endAt!=undefined){
+          sprintTasksDone.push(sprintTasksTot[j]);
+        }
+      }
+
+      item.setTasksTot(sprintTasksTot.length);
+      item.setTasksDone(sprintTasksDone.length);
+    }
+
+
+    //affichage console
+    console.log('nb sprint : '+sprints.length );
+    if(sprints.length>0){
+      console.log('tache terminé premier sprint : '+sprints[0].numTasksDone );
+    }
+    console.log('tache tot : '+totalTaskCount );
+    console.log('debut proj : '+dateStartProject );
+    console.log('fin proj : '+dateEndProject );
+
+
+
     const ctx = document.getElementById('myChart').getContext('2d');
 
-    console.log(`cest un test2 : ${ctx}`);
     const myChart = new Chart(ctx, {
       type: 'bar',
       data: {
@@ -51,6 +169,5 @@ BlazeComponent.extendComponent({
         },
       },
     });
-    console.log('cest un test3');
   },
 }).register('chart');
